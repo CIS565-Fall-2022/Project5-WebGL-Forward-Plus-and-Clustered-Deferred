7717b80593a5b5f3675328ad60f85eaa12a97f68
Shehzan Mohammed <mzshehzanayub@gmail.com>
2021-10-22 22:51:12 -0400

    Fix the build and update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,66 @@
 const path = require('path');
 const webpack = require('webpack');
-const MinifyPlugin = require('babel-minify-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MinifyPlugin = require('terser-webpack-plugin');
 
-module.exports = function(env) {
-  const isProduction = env && env.production === true;
+module.exports = function(env, argv) {
+  const isProduction = argv.mode && argv.mode === 'production';
 
   return {
+    context: __dirname,
     entry: path.join(__dirname, 'src/init'),
     output: {
       path: path.join(__dirname, 'build'),
       filename: 'bundle.js',
     },
     module: {
-      loaders: [
+      rules: [
         {
           test: /\.js$/,
           exclude: /(node_modules|bower_components)/,
-          loader: 'babel-loader',
-          query: {
-            presets: [['env', {
-              targets: {
-                browsers: ['> 1%', 'last 2 major versions'],
-              },
-              loose: true,
-              modules: false,
-            }]],
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: [['@babel/preset-env', {
+                      targets: {
+                          browsers: ['> 1%', 'last 2 major versions'],
+                      },
+                      loose: true,
+                      modules: false,
+                  }]],
+              }
           },
         },
         {
           test: /\.glsl$/,
-          loader: 'webpack-glsl-loader'
+          use: 'webpack-glsl-loader'
         },
       ],
     },
     plugins: [
       isProduction ? new MinifyPlugin({
-        keepFnName: true,
-        keepClassName: true,
+        terserOptions: {
+          keep_fnames: true,
+          keep_classnames: true,
+        }
       }) : undefined,
       new webpack.DefinePlugin({
         'process.env': {
           'NODE_ENV':  (isProduction ? JSON.stringify('production'): JSON.stringify('development')),
         }
       }),
+      new HtmlWebpackPlugin({
+        template: "./index.html"
+      })
     ].filter(p => p),
+    performance: {
+        hints: false,
+        maxEntrypointSize:2048000,
+        maxAssetSize: 2048000
+    },
     devtool: 'source-map',
     devServer: {
-      port: 5650,
-      publicPath: '/build/'
+      port: 5650
     },
   };
 };
