99d6eb93ef7ef31f002bfdc2d247872d111853cb
Ryan Tong <ryanryantong@gmail.com>
2022-11-01 20:17:39 -0400

    nothing changed

--- src/scene.js
+++ src/scene.js
@@ -8,7 +8,8 @@ export const LIGHT_RADIUS = 5.0;
 export const LIGHT_DT = -0.03;
 
 // TODO: This controls the number of lights
-export const NUM_LIGHTS = 500;
+export const NUM_LIGHTS = 2000
+;
 
 class Scene {
   constructor() {
