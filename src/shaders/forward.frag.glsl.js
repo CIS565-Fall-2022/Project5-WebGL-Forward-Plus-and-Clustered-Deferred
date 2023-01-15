411f5432f62428c878708169c6de293e7cce1a1f
Constance Wang <constancezwang@gmail.com>
2023-01-14 22:05:28 -0500

    add notes

--- src/shaders/forward.frag.glsl.js
+++ src/shaders/forward.frag.glsl.js
@@ -16,6 +16,8 @@ export default function(params) {
   varying vec3 v_normal;
   varying vec2 v_uv;
 
+  // TBN transformation of tangent space normal into world normal
+  // uses arbitrary tangent (no tangent buffer)
   vec3 applyNormalMap(vec3 geomnor, vec3 normap) {
     normap = normap * 2.0 - 1.0;
     vec3 up = normalize(vec3(0.001, 1, 0.001));
@@ -30,8 +32,15 @@ export default function(params) {
     vec3 color;
   };
 
+  // index = index of the light (eg. the 5th light -> index = 4)
+  // component = which float you want to access (eg. the light's position's z coord -> component = 2)
+  // assuming layout | pos.x pos.y pos.z radius | col.r col.g col.b |
+  //                 pixel0                     pixel1
+  //                 | pc 0  pc 1  pc 2  pc 3  | pc 0  pc 1   pc 2  |
   float ExtractFloat(sampler2D texture, int textureWidth, int textureHeight, int index, int component) {
-    float u = float(index + 1) / float(textureWidth + 1);
+    // Get pixel x and y coords as fraction (aka normalized texture coords from 0 to 1)
+    // + 1 to offset the 0-indexing
+    float u = float(index + 1) / float(textureWidth + 1); 
     int pixel = component / 4;
     float v = float(pixel + 1) / float(textureHeight + 1);
     vec4 texel = texture2D(texture, vec2(u, v));
@@ -50,6 +59,11 @@ export default function(params) {
   Light UnpackLight(int index) {
     Light light;
     float u = float(index + 1) / float(NUM_LIGHTS + 1);
+     // this only works because we loaded texture of width=NUM_LIGHTS, height=2
+     // would change if we add more light info
+     // it's seriously the most disgusting code I've seen in a long time
+     // how is it more jank than cuda, what's the point of using a 
+     // higher level language if it can't at least make the code less jank
     vec4 v1 = texture2D(u_lightbuffer, vec2(u, 0.0));
     vec4 v2 = texture2D(u_lightbuffer, vec2(u, 0.5));
     light.position = v1.xyz;
@@ -77,10 +91,12 @@ export default function(params) {
   void main() {
     vec3 albedo = texture2D(u_colmap, v_uv).rgb;
     vec3 normap = texture2D(u_normap, v_uv).xyz;
-    vec3 normal = applyNormalMap(v_normal, normap);
+    vec3 normal = applyNormalMap(v_normal, normap); // now it's a world normal
 
     vec3 fragColor = vec3(0.0);
 
+    // very simple light calculations added together. Lambert with distance falloff
+    // no occlusion considered, all lights just shine straight through the wall...
     for (int i = 0; i < NUM_LIGHTS; ++i) {
       Light light = UnpackLight(i);
       float lightDistance = distance(light.position, v_position);
